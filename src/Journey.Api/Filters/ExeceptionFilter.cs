db7730ff22568d48fb70b48cd14b0090f639f192
jorgeuluiz <jorgeuluiz@gmail.com>
2024-07-10 15:33:23 -0300

    Primeiro commit

---
+++ src/Journey.Api/Filters/ExeceptionFilter.cs
@@ -0,0 +1,35 @@
+﻿using Journey.Communication.Responses;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Journey.Api.Filters
+{
+    public class ExeceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is JourneyException)
+            {
+                var journeyException = context.Exception as JourneyException;
+                context.HttpContext.Response.StatusCode = (int)journeyException.GetStatusCode();
+
+                var reponseJson = new ResponseErrorsJson(journeyException.GetErrorMessages());
+
+                context.Result = new ObjectResult(reponseJson);
+            }           
+            else
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var list = new List<string>
+                {
+                    "Erro desconhecido"
+                };
+
+                var reponseJson = new ResponseErrorsJson(list);
+                context.Result = new ObjectResult(reponseJson);
+            }
+        }
+    }
+}
---
+++ src/Journey.Application/UseCases/Trips/GetAll/GetAllTripsUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Journey.Communication.Responses;
+using Journey.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Application.UseCases.Trips.GetAll
+{
+    public class GetAllTripsUseCase
+    {
+        public ResponseTripsJson Execute()
+        {           
+
+            var dbContext = new JourneyDbContext();
+
+            var trips = dbContext
+                .Trips
+                .Include(trip => trip.Activities)
+                .ToList();
+
+
+            return new ResponseTripsJson
+            {
+                Trips = trips.Select(trip => new ResponseShortTripJson
+                {
+                    Id = trip.Id,
+                    EndDate = trip.EndDate,
+                    Name = trip.Name,
+                    StartDate = trip.StartDate
+
+                }).ToList()
+            };
+        }
+    }
+}
